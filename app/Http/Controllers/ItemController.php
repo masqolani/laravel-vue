5ab61bed4f6cde7b8a746e7e854cd5157a3acb77
asqolani <asqolani@pergi.com>
2017-10-06 14:02:00 +0700

    first commit github

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,106 @@
+<?php
+
+// ItemController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Item;
+use Log;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::all();
+
+        log::info('items => '.print_r($items,true));
+
+        return response()->json($items);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Item([
+          'name' => $request->get('name'),
+          'price' => $request->get('price')
+        ]);
+        $item->save();
+        return response()->json('Successfully added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = Item::find($id);
+        return response()->json($item);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+        $item->name = $request->get('name');
+        $item->price = $request->get('price');
+        $item->save();
+
+        return response()->json('Successfully Updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $item = Item::find($id);
+      $item->delete();
+
+      return response()->json('Successfully Deleted');
+    }
+}
